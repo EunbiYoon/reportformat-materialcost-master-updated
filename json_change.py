0dff8173918203a0c8fdaf8ec0676a35ff90e538
Eunbi Yoon <yinyinbei0717@gmail.com>
2023-05-09 10:07:55 -0500

    werwer

--- json_change.py
+++ json_change.py
@@ -7,23 +7,20 @@ from jsonmerge import merge
 from jsonmerge import Merger
 
 read_excel=pd.read_excel("C:/Users/RnD Workstation/Documents/NPTGERP/graph.xlsx")
+read_excel.index=read_excel['Unnamed: 0']
+read_excel=read_excel.drop(['Unnamed: 0'],axis=1)
 
 #column_list
 column_list=list(read_excel.columns)
-column_json=json.dumps(column_list)
 
 #index_list
-index_list=list(read_excel['Unnamed: 0'])
-index_json=json.dumps(index_list)
+index_list=list(read_excel.index)
 
 #value_list
-value_list=str(list(read_excel.values)).replace('array(', '').replace(", dtype=object)","")
+value_list=read_excel.values.round(1).tolist()
 
 #json file format
-columns=column_list
-rows=index_list
-values=value_list
-json_object={"columns":columns, "index":rows, "values":value_list}
+json_object={"columns":column_list, "index":index_list, "values":value_list}
 jj=json.dumps(json_object)
 print(jj)
 
